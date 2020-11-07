88503792b1f7427b0c463f9f7112204f6dabea48
A-barr98 <A-barr98>
2020-11-07 15:25:39 +0000

    add first class

---
+++ classes/TeamMember.js
@@ -0,0 +1,20 @@
+class TeamMember {
+  constructor(name, year) {
+    this.name = '';
+    this.email = '';
+    this.interets1 = '';
+    this.interest2 = '';
+    this.interest3 = '';
+  }
+ SetName(str_name){
+    this.name = str_name
+ }
+ SetEmail(str_email){
+    this.email = str_email 
+ }
+ SetInterests(interest1, interest2, interest3){
+    this.interest1 = interest1
+    this.interest2 = interest2
+    this.interest3 = interest3
+ }
+}
